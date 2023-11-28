f0d86a857a287ec2f0d5af50b0e00d055806a55d
Liam Bourgoin <liam.bourgoin11@gmail.com>
2023-11-28 17:12:29 +0100

    Controller project et debut d'une card-project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,26 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    if @project.save
+      redirect_to project_path(@project), notice: "Projet créé avec succès."
+    else
+      render :new, status: :unprocessable_entity
+      puts "Erreur lors de l'enregistrement du projet : #{project.errors.full_messages.join(', ')}"
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
