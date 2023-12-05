49516f86c40bd5893634648b0ffe6f583c29e6f3
Liam Bourgoin <liam.bourgoin11@gmail.com>
2023-12-05 14:41:49 +0100

    preload music

--- app/javascript/shared/debugFunctions.js
+++ app/javascript/shared/debugFunctions.js
@@ -1,21 +1,21 @@
 // debugFunctions.js
 
-  // Afficher les tiles qui ont la propriété collides = true
-  export function showDebugWalls() {
-    const debugGraphics = this.add.graphics().setAlpha(0.7);
+// Afficher les tiles qui ont la propriété collides = true
+export function showDebugWalls() {
+  const debugGraphics = this.add.graphics().setAlpha(0.7);
 
-    // Iterate through each tile in the ground layer
-    this.groundLayer.forEachTile(tile => {
-      // Check if the tile has the property 'collides' set to true
-      if (tile.properties.collides === true) {
-        debugGraphics.fillStyle(0xE6E6E6, 1); // Set the colliding tile color
-        debugGraphics.fillRect(tile.pixelX, tile.pixelY, tile.width, tile.height);
-      }
-    }, this);
+  // Iterate through each tile in the ground layer
+  this.groundLayer.forEachTile((tile) => {
+    // Check if the tile has the property 'collides' set to true
+    if (tile.properties.collides === true) {
+      debugGraphics.fillStyle(0xe6e6e6, 1); // Set the colliding tile color
+      debugGraphics.fillRect(tile.pixelX, tile.pixelY, tile.width, tile.height);
+    }
+  }, this);
 
-    // Remove the default debug rendering method
-    this.groundLayer.renderDebug(debugGraphics, {
-      tileColor: null,
-      collidingTileColor: null,
-    });
-  }
+  // Remove the default debug rendering method
+  this.groundLayer.renderDebug(debugGraphics, {
+    tileColor: null,
+    collidingTileColor: null,
+  });
+}
